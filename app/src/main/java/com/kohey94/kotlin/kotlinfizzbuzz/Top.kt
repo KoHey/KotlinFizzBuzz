0187d098d0f1f248f755f69876b0c9481f4ede24
KoHey <sugawara622@gmail.com>
2017-02-18 23:53:22 +0900

    トップ画面追加

--- app/src/main/java/com/kohey94/kotlin/kotlinfizzbuzz/Top.kt
+++ app/src/main/java/com/kohey94/kotlin/kotlinfizzbuzz/Top.kt
@@ -1,5 +1,7 @@
 package com.kohey94.kotlin.kotlinfizzbuzz
 
+import android.content.Intent
+import android.content.IntentFilter
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -9,10 +11,10 @@ class Top : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_top)
-        val kotlinButton = findViewById(R.id.createButton) as Button
-        kotlinButton.setAllCaps(false)
+        val kotlinButton = findViewById(R.id.kotlinButton) as Button
         kotlinButton.setOnClickListener { v ->
-            //画面遷移
+            val intent = Intent(application, MainActivity::class.java)
+            startActivity(intent)
         }
     }
 }
